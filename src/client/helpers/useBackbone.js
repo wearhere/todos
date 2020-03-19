56e6b693952489eb97ad4daee3b4ccf272a1e23a
Jeff Wear <jeff@mixmax.com>
2020-03-19 01:34:33 -0700

    Sync the backbone helpers with the master copy on Glitch

--- src/client/helpers/useBackbone.js
+++ src/client/helpers/useBackbone.js
@@ -1,32 +1,37 @@
-import { useRef } from 'react';
+// TODO(jeff): This file is a copy of https://glitch.com/edit/#!/use-backbone.
+// Import this logic from npm once it has been published there.
+import { useEffect } from 'react';
 import useForceUpdate from 'use-force-update';
 
-// TODO(jeff): Replace with the tested version from https://glitch.com/edit/#!/use-backbone
-// once Glitch comes back up.
-export function useCollection(collection) {
-  const forceUpdate = useForceUpdate();
+// TODO: Let the user customize the events observed, for instance to restrict updates
+// to certain attributes changing, or to update on custom events.
+export function useModel(model) {
+  const update = useForceUpdate();
+  useEffect(() => {
+    // We only need to observe `change` because Backbone will fire `change` events
+    // if syncs result in changes.
+    model.on('change', update);
+    return () => model.off('change', update);
+  }, [model, update]);
 
-  const collectionRef = useRef(null);
-  const previousCollection = collectionRef.current;
-  if (!previousCollection || (previousCollection !== collection)) {
-    if (previousCollection) previousCollection.off('update change', forceUpdate);
-    collection.on('update change', forceUpdate);
-    collectionRef.current = collection;
-  }
-
-  return collection;
+  // Give the client easy access to the model's attributes.
+  return {...model.attributes};
 }
 
-export function useModel(model) {
-  const forceUpdate = useForceUpdate();
+// TODO: Let the user customize the events observed, for instance to update on custom events.
+export function useCollection(collection) {
+  const update = useForceUpdate();
+  useEffect(() => {
+    // `reset` silences `update`, otherwise the latter covers adds/removes.
+    // syncs will fire those events, so we don't need to observe that.
+    // We also proxy change events from the collection items so that clients don't
+    // have to observe them directly.
+    collection.on('update reset sort change', update);
+    return () => collection.off('update reset sort change', update);
+  }, [collection, update]);
 
-  const modelRef = useRef(null);
-  const previousModel = modelRef.current;
-  if (!previousModel || (previousModel !== model)) {
-    if (previousModel) previousModel.off('change', forceUpdate);
-    model.on('change', forceUpdate);
-    modelRef.current = model;
-  }
-
-  return { ...model.attributes, model };
+  // Backbone proxies to Underscore.js to provide Array-like functions
+  // on the collection, so it's convenient for the client to use the collection
+  // directly (contrast `useModel`'s return value).
+  return collection;
 }
