9ec5d4a242630f67565b4aa0ced99788e2446aa6
Jeff Wear <jeff@mixmax.com>
2020-03-18 19:04:38 -0700

    Initialize a web server

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('hello world!');
+const server = require('./src/server');
+
+const PORT = process.env.PORT || '3000';
+server.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
