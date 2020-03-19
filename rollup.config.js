badc95d264b1b2f17a700596e4d71a65df3367c4
Jeff Wear <jeff@mixmax.com>
2020-03-18 19:11:18 -0700

    Initialize frontend
    Server livereloading has been broken and will be fixed in a subsequent commit.

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: 'src/client/main.js',
+  output: {
+    file: 'public/bundle.js',
+    format: 'iife'
+  }
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// This lets webpages source assets relative to this directory.
+// Example: `src="bundle.js"` => `public/bundle.js`.
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 module.exports = app;
